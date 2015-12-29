78113436b5d4e9bb8ec534cdb373f2ff11b0a848
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-28 19:19:21 -0800

    Updating MapsTest

--- src/test/java/sir/wellington/alchemy/collections/sets/SetsTest.java
+++ src/test/java/sir/wellington/alchemy/collections/sets/SetsTest.java
@@ -42,7 +42,7 @@ import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThr
  *
  * @author SirWellington
  */
-@Repeat(10)
+@Repeat(100)
 @RunWith(AlchemyTestRunner.class)
 public class SetsTest
 {
